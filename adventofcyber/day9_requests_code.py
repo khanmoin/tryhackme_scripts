333194360696edb2285c4b10a8dad7fc214b1f6d
sux0r <sux0r@git.com>
2020-06-04 08:52:54 -0400

    adding python script to solve day 9 challenge

--- adventofcyber/day9_requests_code.py
+++ adventofcyber/day9_requests_code.py
@@ -1,16 +1,20 @@
+"""
+    Python script to get flag for the day 9 challenge of advent of cyber
+"""
+
 import requests
+
 
 path = '/'
 host = 'http://10.10.169.100:3000'
 flag = ''
-json = {'value' : '', 'next' : ''}
-#response = requests.get(host + path)
-#json = response.json()
-#flag += json['value']
+json = {'value' : '', 'next' : ''}          # dummy values since we know the response
+
 
 while json['next'] != 'end':
     response = requests.get(host + '/' + json['next'])
     json = response.json()
+    # remove end from the flag
     if json['value'] != 'end':
       flag += json['value']
 
